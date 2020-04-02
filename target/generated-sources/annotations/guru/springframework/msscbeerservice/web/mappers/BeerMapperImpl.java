50d612453628959f9e5333a5a69c5f19d7d6cece
bcasalvieri <bmcasalvieri@gmail.com>
2020-04-02 16:26:43 -0400

    fixing failing tests. closes #10

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-02T16:17:48-0400",
+    date = "2020-04-02T16:22:22-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.6 (Oracle Corporation)"
 )
 @Component
