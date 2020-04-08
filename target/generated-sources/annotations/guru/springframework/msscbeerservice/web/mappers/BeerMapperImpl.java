efe334339e712e17a7c1d84964dba4bc54d4f475
bcasalvieri <bmcasalvieri@gmail.com>
2020-04-08 13:21:46 -0400

    adding endpoint to get beer by upc

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-08T13:10:32-0400",
+    date = "2020-04-08T13:19:32-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.6 (Oracle Corporation)"
 )
 @Component
