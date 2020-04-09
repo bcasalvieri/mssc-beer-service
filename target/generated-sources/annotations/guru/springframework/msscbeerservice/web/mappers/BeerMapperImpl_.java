2f25c4bb715fbd7b3894ab868c3328c2e7a2ad8e
bcasalvieri <bmcasalvieri@gmail.com>
2020-04-09 11:34:38 -0400

    correcting hibernate error

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-09T10:54:32-0400",
+    date = "2020-04-09T11:29:37-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.6 (Oracle Corporation)"
 )
 @Component
